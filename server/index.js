72c767930651ebe8b0e0380db27194391f20653f
Diana Vitanyi <>
2023-08-25 16:54:29 +0200

    server: posts route basic setup

--- server/index.js
+++ server/index.js
@@ -3,8 +3,12 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 
 const app = express();
+
+app.use('/posts', postRoutes);
 
 // NOTE: could use express.json() and express.urlencoded() middleware functions directly
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
@@ -14,7 +18,8 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 // NOTE: we are calling the cors function and passing its return value (which is a middleware function) into app.use
 
+
 const CONNECTION_URL = "mongodb+srv://dianavitanyi:lTxK82BqL0H8b4sl@cluster0.lqwki7o.mongodb.net/?retryWrites=true&w=majority";
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 4000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => app.listen(PORT, () => { console.log(`The server is running on port: ${PORT}.`) })).catch((err) => console.log(err));
